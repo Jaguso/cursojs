0b3c807cb73ea47b2841345c86874917ae5f28e1
Jaguso <jagutzo@gmail.com>
2020-05-23 19:02:24 -0500

    Added exercises

--- homework.js
+++ homework.js
@@ -27,3 +27,67 @@ function positiveSum(arr) {
 
 }
 
+
+// EJERCICIO 4
+// Create a function that returns the sum of the two lowest positive numbers given an array of minimum 4 positive integers. 
+// No floats or non-positive integers will be passed.
+// For example, when an array is passed like [19, 5, 42, 2, 77], the output should be 7.
+// [10, 343445353, 3453445, 3453545353453] should return 3453455.
+
+function sumTwoSmallestNumbers(numbers) {  
+ 
+}
+
+// EJERCICIO 5
+// Define a function such that each lowercase letter becomes uppercase and each uppercase letter 
+// becomes lowercase. The function has a string as input.
+
+
+function toAlternatingCase(str) {
+
+}
+
+
+
+// EJERCICIO 6
+// The code gives an error! You have to fix it
+a = 123.toString
+
+
+
+// EJERCICIO 7
+// This function takes two numbers as parameters, the first number being the coefficient, and the second number being the exponent.
+
+// Your function should multiply the two numbers, and then subtract 1 from the exponent. Then, it has to print out an expression 
+// (like 28x^7). "^1" should not be truncated when exponent = 2.
+
+// For example:
+
+// derive(7, 8)
+// In this case, the function should multiply 7 and 8, and then subtract 1 from 8. It should output "56x^7", the first number 
+// 56 being the product of the two numbers, and the second number being the exponent minus 1.
+
+derive(7, 8) // --> this should output "56x^7" 
+derive(5, 9) // --> this should output "45x^8" 
+
+function derive(coefficient, exponent) {
+    
+}
+
+
+
+// EJERCICIO 8
+// Your task is to find the first element of an array that is not consecutive.
+// By not consecutive we mean not exactly 1 larger than the previous element of the array.
+// E.g. If we have an array [1,2,3,4,6,7,8] then 1 then 2 then 3 then 4 are all consecutive but 6 is not, so that's the first 
+// non-consecutive number.
+
+// If the whole array is consecutive then return null2.
+
+// The array will always have at least 2 elements1 and all elements will be numbers. The numbers will also all be unique and 
+// in ascending order. The numbers could be positive or negative and the first non-consecutive could be either too!
+
+
+function firstNonConsecutive (arr) {
+
+}
